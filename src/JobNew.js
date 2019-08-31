73381d6fafe560c3f1767892845a22082d5b45b8
GOH JUN HAN <1801397@sit.singaporetech.edu.sg>
2019-09-01 03:39:43 +0800

    test

--- src/JobNew.js
+++ src/JobNew.js
@@ -4,7 +4,11 @@ import {setStateByPath,getState,setById,getMaxId} from './fixtures'
 function JobNew(props){
 
 	const jobs = props.jobs || {}
-  	const [text, setText] = React.useState(jobs.text || '')
+	const [title, setTitle] = React.useState(jobs.title || '')
+	const [description, setDesc] = React.useState(jobs.description || '')
+	const [job_type, setJobType] = React.useState(jobs.job_type || '')
+	const [location, setLocation] = React.useState(jobs.location || '')
+
   	//const todayInt = fromDateToInt(new Date())
   	const handleSave = () => {
     if (props.jobs) {
@@ -36,34 +40,34 @@ function JobNew(props){
 							<div class="col-md-6">
 								<h5 class="title text-success">Step 1: Create Job Name</h5>
 								<div class="form-group">
-									<input type="text" class="form-control form-control-alternative"id="exampleFormControlInput1" placeholder="Job Name..">
+									<input type="text" class="form-control form-control-alternative"id="exampleFormControlInput1" placeholder="Job Name.." value={title} onChange={(e) => setTitle(e.target.value)}>
 									</input>
 								</div>
 								<h5 class="title text-success">Step 2: Select JobType</h5>
 								<div class="custom-control custom-radio mb-3">
-								<input name="custom-radio-1" class="custom-control-input" id="customRadio1" type="radio"></input>
+								<input name="custom-radio-1" class="custom-control-input" id="customRadio1" type="radio" value={job_type} onChange={(e) => setJobType(e.target.value)}></input>
 								<label class="custom-control-label" for="customRadio1">Research & Development</label>
 								</div>
 								<div class="custom-control custom-radio mb-3">
-								<input name="custom-radio-1" class="custom-control-input" id="customRadio2" type="radio"></input>
+								<input name="custom-radio-1" class="custom-control-input" id="customRadio2" type="radio" value={job_type} onChange={(e) => setJobType(e.target.value)}></input>
 								<label class="custom-control-label" for="customRadio2">Freelance</label>
 								</div>
 								<h5 class="title text-success">Step 3: Write A short Brief of the Job</h5>
 								<div class="form-group">
 									<form>
-										<textarea class="form-control form-control-alternative" id="shortDes1" rows="3" placeholder="Short Description.."></textarea>
+										<textarea class="form-control form-control-alternative" id="shortDes1" rows="3" placeholder="Short Description.." value={description} onChange={(e) => setDesc(e.target.value)}></textarea>
 									</form>
 								</div>
 								<h5 class="title text-success">Step 4: Add A Venue</h5>
 								<div class="form-group">
 									<form>
-										<textarea class="form-control form-control-alternative" id="venue1" rows="3" placeholder="Venue.."></textarea>
+										<textarea class="form-control form-control-alternative" id="venue1" rows="3" placeholder="Venue.." value={location} onChange={(e) => setLocation(e.target.value)}></textarea>
 									</form>
 								</div>	
 							</div>
 						</div>			
 					</div>
-				<a href="#" class="btn btn-success mt-4" onClick={addJob} >Add Job</a>
+				<a href="#" class="btn btn-success mt-4" onClick={handleSave} >Add Job</a>
             </div>
 			</div>
 			</div>
